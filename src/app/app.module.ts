5a8ac0995d4d96e0f58545f6c48027ff39bb4617
shubhamj974 <jagdaleshubham036@gmail.com>
2023-08-05 11:36:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieposterComponent } from './shared/components/movieposter/movieposter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieposterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/components/movieposter/movieposter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movieposter',
+  templateUrl: './movieposter.component.html',
+  styleUrls: ['./movieposter.component.scss']
+})
+export class MovieposterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
